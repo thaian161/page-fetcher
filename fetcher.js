464d031086df5bcbbff92420676a775299defa0e
thaian161 <thaian161@yahoo.com>
2022-06-09 00:26:49 -0500

    add comment for future use

--- fetcher.js
+++ fetcher.js
@@ -1,24 +1,28 @@
-const input = process.argv.slice(2);
+///// Take two command line arguments
+///////  1. a URL
+///////  2. a local file path
+///// Download the resource at URL to the local path
+///// Upon completion print "Downloaded and saved 1235 bytes to ./index.html"
+
+//DEMO
+//input to node: node fetcher.js http://www.example.edu/ ./index.html
+//output/result: Downloaded and saved 3261 bytes to ./index.html
+
 const request = require('request');
 const fs = require('fs');
-const readline = require('readline');
 
-//Create interface rl boilerplate and define input output
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const input = process.argv.slice(2);
 
-//Define URL as index 0 and path as index 1 of the new args.slice(2) from line 24
 request(`${input[0]}`, (error, response, body) => {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for whatever URL that passed in input[0]
+  console.log('body:', body); // Print the HTML from input[0]
 
   fs.writeFile(`${input[1]}`, body, (err) => {
     if (err) {
       console.error(err);
     } else {
+      //File written successfully
       //Upon completion, it should print out a message
       console.log(`Downloaded and save ${body.length} bytes to ${input[1]}`);
       process.exit();
